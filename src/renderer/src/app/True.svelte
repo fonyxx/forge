723f965f2931173261c805042ecff2ee2c44e825
xlang <xlangk@proton.me>
2023-03-08 22:36:06 -0500

    Heavy UI changes

--- src/renderer/src/app/True.svelte
+++ src/renderer/src/app/True.svelte
@@ -1,10 +1,66 @@
 <script lang="ts">
   import UserGrid from "../components/UserGrid/UserGrid.svelte";
+
+  let editorAvailable = true;
+  let contentSceneAvailable = true;
+
+  let mouseX = 0, mouseY = 0, mouseOn = false;
+
+  function setSceneMode(mode: boolean = !contentSceneAvailable) {
+    if (contentSceneAvailable) {
+      editorAvailable = false;
+      setTimeout(() => {
+        contentSceneAvailable = mode;
+      }, 400);
+    } else {
+      contentSceneAvailable = mode;
+      setTimeout(() => {
+        editorAvailable = true;
+      }, 400);
+    }
+  }
 </script>
 
 <div class="true">
-  <UserGrid
-  />
+  <div class="top">
+
+  </div>
+
+  <div class={"body" + (!contentSceneAvailable ? " disable-scene" : "")}>
+    <div class="side-bar">
+      <div class="inner">
+        <button on:click={() => setSceneMode(false)}>Toggle Scene</button>
+      </div>
+    </div>
+
+    <div class="editor-area">
+      <div class="tabs">
+
+      </div>
+
+      <div
+        class={"view" + (!editorAvailable ? " disable-editor" : "")}
+        on:mouseenter={() => mouseOn = true} on:mouseleave={() => mouseOn = false}
+        on:mousemove={(e) => { mouseX = e.clientX; mouseY = e.clientY; }}
+      >
+        <UserGrid />
+      </div>
+    </div>
+  </div>
+
+  <div class={"document" + (contentSceneAvailable ? " disable-document" : "")}>
+    Document
+  </div>
+
+  <div class="gutter">
+    <div class="inner">
+      <section class="left">
+        <div class={"nugget" + (!mouseOn ? " hide-prop" : "")}>
+          ({mouseX}, {mouseY})
+        </div>
+      </section>
+    </div>
+  </div>
 </div>
 
 <style lang="scss">
@@ -15,5 +71,164 @@
     width: 100vw;
     display: flex;
     flex-direction: column;
+
+    .top {
+      display: flex;
+      height: calc(50px + 30px);
+      background: $l0;
+      min-height: calc(50px + 30px);
+      width: 100vw;
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 1;
+    }
+
+    .document {
+      position: fixed;
+      top: calc(50px + 30px);
+      left: 0;
+      width: 100vw;
+      height: calc(100vh - (50px + 30px) - 40px);
+      background: $l0;
+      transition: opacity 200ms, top 200ms, transform 200ms, filter 200ms;
+
+      &.disable-document {
+        pointer-events: none;
+        opacity: 0;
+        top: calc(50px + 30px + 100px);
+        transform: scale(0.9);
+        filter: blur(100px) brightness(5);
+      }
+    }
+
+    .body {
+      height: calc(100vh - (50px + 30px) - 40px);
+      width: 100vw;
+      display: flex;
+      flex-direction: row;
+      transition: opacity 200ms, top 200ms, transform 200ms, filter 200ms;
+      position: fixed;
+      top: calc(50px + 30px);
+      left: 0;
+
+      &.disable-scene {
+        pointer-events: none;
+        opacity: 0;
+        top: -100px;
+        transform: scale(0.9);
+        filter: blur(100px) brightness(5);
+      }
+
+      .side-bar {
+        width: 320px;
+        height: 100%;
+        background: $l0;
+        min-width: 320px;
+        transition: 100ms;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .inner {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          width: calc(100% - 10px);
+          height: calc(100% - 10px);
+          background: $l1;
+          border-radius: 4px;
+        }
+      }
+
+      .editor-area {
+        display: flex;
+        flex-direction: column;
+        width: calc(100vw - 320px - 10px);
+        height: 100%;
+        overflow: hidden;
+        transition: 100ms;
+        border-radius: 4px;
+
+        .tabs {
+          height: 30px;
+          display: flex;
+          flex-direction: row;
+          background: $l0;
+          min-height: 30px;
+          width: 100%;
+        }
+
+        .view {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 100%;
+          height: calc(100vh - (50px + 30px) - 40px - 30px - (10px / 2));
+          transition: opacity 200ms, transform 200ms, border-width 200ms, border-radius 200ms;
+          border: 0px solid $ct0;
+          border-radius: 4px;
+          overflow: hidden;
+
+          &.disable-editor {
+            pointer-events: none;
+            opacity: 0;
+            transform: scale(0.9);
+            border-width: 25vw;
+            border-radius: 100%;
+            transition: opacity 200ms, transform 200ms, border-width 200ms, border-radius 4000ms;
+          }
+        }
+      }
+    }
+
+    .gutter {
+      height: 40px;
+      width: 100vw;
+      background: $l0;
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .inner {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        width: calc(100% - 10px);
+        height: calc(100% - 10px);
+        background: $l1;
+        border-radius: 4px;
+
+        .nugget {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: calc(40px - 10px - 10px);
+          padding: 0 5px;
+          text-align: center;
+          border-radius: 2px;
+          position: relative;
+
+          &:nth-child(1) {
+            margin-left: 5px;
+          }
+
+          &:after {
+            display: flex;
+            content: "";
+            position: absolute;
+            right: -5px;
+            width: 1px;
+            height: calc(40px - 10px - 10px - 5px);
+            background: $d0;
+          }
+        }
+      }
+    }
   }
 </style>
--- src/renderer/src/components/UserGrid/UserGrid.svelte
+++ src/renderer/src/components/UserGrid/UserGrid.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-  import BarAxis from "./BarAxis.svelte";
   import Grid from "./Grid.svelte";
   import {onDestroy, onMount} from "svelte";
 
-  let width = 500, height = 500, lineThickness, spacing;
-  let mouseX = 0, mouseY = 0, mouseDown = false;
-  let x = 0, y = 0, mouseXDrag = 0, mouseYDrag = 0, zoom = 1;
+  export let cornerRounding = 2;
+  export let lineThickness = 1;
+  export let spacing = 20;
+  export let lineColor = "#111111";
+  export let backgroundColor = "#ffffff";
+
+  let width = 500, height = 500;
+  let mouseX = 0, mouseY = 0, mouseDown = false, mouseDownButtonID = 0;
+  let x = 0, y = 0, mouseXDrag = 0, mouseYDrag = 0, zoom = 4;
 
   let keyDown = false, keyName = "";
 
@@ -13,12 +18,18 @@
 
   onMount(() => {
     window.addEventListener("wheel", scrollListener = (e) => {
-      if (keyDown && keyName === "Control") {
-        zoom += e.deltaY / 1000;
+      if (keyDown && keyName === "Control" || keyDown && keyName === "Alt") {
+        zoom += -e.deltaY / (keyName == "Alt" ? 500 : 2000);
 
         if (zoom < 0.1) {
           zoom = 0.1;
         }
+
+        zoom = Math.round(zoom * 100) / 100;
+        zoom = Math.max(0.7, zoom);
+
+        x = mouseX - (mouseX - x) / zoom;
+        y = mouseY - (mouseY - y) / zoom;
       }
     });
   });
@@ -28,7 +39,7 @@
   });
 </script>
 
-<div tabindex="0" class="user-grid" bind:clientWidth={width} bind:clientHeight={height} on:keydown={(e) => {
+<div autofocus tabindex="0" class="user-grid" bind:clientWidth={width} bind:clientHeight={height} on:keydown={(e) => {
     keyDown = true;
     keyName = e.key;
   }} on:keyup={() => {
@@ -39,18 +50,24 @@
     mouseX = e.clientX;
     mouseY = e.clientY;
 
-    if (mouseDown) {
-      x += mouseX - mouseXDrag;
-      y += mouseY - mouseYDrag;
+    if (mouseDown && mouseDownButtonID === 1) {
+        x += mouseX - mouseXDrag;
+        y += mouseY - mouseYDrag;
 
-      mouseXDrag = mouseX;
-      mouseYDrag = mouseY;
+        mouseXDrag = mouseX;
+        mouseYDrag = mouseY;
     }
-  }} on:mousedown={() => {
+  }} on:mousedown={(e) => {
     mouseDown = true;
 
     mouseXDrag = mouseX;
     mouseYDrag = mouseY;
+
+    // button ID
+    // 0: left
+    // 1: middle
+    // 2: right
+    mouseDownButtonID = e.button;
   }} on:mouseup={() => {
     mouseDown = false;
 
@@ -62,8 +79,13 @@
     mouseXDrag = 0;
     mouseYDrag = 0;
   }}>
-    <Grid spacing={50 * zoom} lineThickness={1 * zoom} width={width} height={height} cellRounding={0 * zoom} x={x % (50 * zoom)} y={y % (50 * zoom)} />
-    <p>{x}, {y}, {mouseX}, {mouseY}, {mouseDown} K: {keyDown} KK: {keyName}</p>
+    <Grid
+      spacing={spacing * zoom} lineThickness={lineThickness}
+      width={width} height={height}
+      cellRounding={cornerRounding * zoom}
+      x={x % (spacing * zoom)} y={y % (spacing * zoom)}
+      lineColor={lineColor} backgroundColor={backgroundColor}
+    />
   </div>
 </div>
 
@@ -85,12 +107,6 @@
       left: 0;
       width: 100%;
       height: 100%;
-    }
-
-    p {
-      position: absolute;
-      top: 0;
-      left: 0;
     }
   }
 </style>
